83dd28cdd4fb9aafd6dcc12c1351476350aa76c1
lxy <C@lxydeMac-mini.local>
2016-09-20 11:22:49 +0800

    create binding

--- app/src/main/java/com/lxy/bind/MainActivity.java
+++ app/src/main/java/com/lxy/bind/MainActivity.java
@@ -1,13 +1,40 @@
 package com.lxy.bind;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.lxy.bind.BR;
+import com.lxy.bind.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        ViewDataBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mUser = new User("lxy", "22");
+
+        binding.setVariable(BR.User, mUser);
+        binding.setVariable(BR.presenter, new Persenter());
+
     }
+
+
+    public class Persenter {
+
+        public void onClick(View view) {
+
+            Toast.makeText(MainActivity.this, mUser.name, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/lxy/bind/User.java
@@ -0,0 +1,16 @@
+package com.lxy.bind;
+
+/**
+ * Created by C on 16/9/20.
+ */
+public class User {
+
+    public String name;
+    public String age;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
